8c0d7519e71e284fae883ff22ca8865c53e76f08
Suman Saurabh <saurabh.kaystha@gmail.com>
2019-10-23 12:01:08 +0530

    Initial version of apptwo generated by JHipster-6.3.1

---
+++ src/main/java/com/jhipster/apptwo/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.jhipster.apptwo.config.audit;
---
+++ src/main/java/com/jhipster/apptwo/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.jhipster.apptwo.web.rest.vm;
